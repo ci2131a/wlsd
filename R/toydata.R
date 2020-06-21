35689a4f3a0e915caf0d82854180c0f52e575ca4
Charles <ci2131a@american.edu>
2020-06-21 14:21:57 -0400

    update data to vignette

--- R/toydata.R
+++ R/toydata.R
@@ -16,13 +16,15 @@ eventdata <- data.frame(event1 = c(0,0,0,1,1),
 
 
 
-lbp <- data.frame(sid = c(1,1,1,2,2,3),
-                  base = c(0,0,0,10,10,15),
-                  follow = c(63,128,156,32,59,28),
-                  status = c(0,0,1,0,0,1),
-                  var1 = c(10,10,10,6,6,3),
-                  var2 = c(1,2,3,1,2,1))
+table1 <- mydata <- data.frame(sid = c(1,1,1,2,2,3),
+                              start = c(0,31,64,0,33,0),
+                              stop = c(31,64,126,33,59,28),
+                              status = c(0,0,1,0,0,1))
 
 
-
-
+table2 <- mydata <- data.frame(sid = c(1,1,1,2,2,3),
+                              start = c(0,31,64,0,33,0),
+                              stop = c(31,64,96,33,59,28),
+                              status = c(0,0,1,0,0,1),
+                              sex = c(0,0,0,1,1,0),
+                              age = c(46,79,0,8766,8799,0))
