4c34a4ae3ae562127f006b61fef23a3fe71eb17d
C <ci2131a@american.edu>
2020-06-30 13:04:06 -0400

    translate table data to toydata

--- R/toydata.R
+++ R/toydata.R
@@ -15,6 +15,10 @@ table2 <- data.frame(id = c(1,1,1,2,2,3),
                               age = c(46,46,46,39,39,57),
                               meds = c(0,1,2,0,1,2))
 
-table3 <- data.frame(id = c(1,2,3),
-                              time = c(96,33,28),
-                              event = c(1,0,1))
+table3 <- data.frame(sid = c(1,2,3),
+                    time1 = c(0,0,0),
+                    time2 = c(96,33,28),
+                    status = c(1,0,1))
+
+table4 <- data.frame(time = c(96,33,28),
+                    status = c(1,0,1))
