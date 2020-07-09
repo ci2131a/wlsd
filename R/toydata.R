e3877d0fc2e2bae6ab96c0ed9f11c4805b73f393
Charles <ci2131a@american.edu>
2020-07-09 19:35:41 -0400

    make toy data the same

--- R/toydata.R
+++ R/toydata.R
@@ -1,30 +1,32 @@
 # toy data sets
-# to use to test the functions
+# use to test the functions
 
-
-table1 <- data.frame(id = c(1,1,1,2,2,3),
-                              time1 = c(0,31,64,0,33,0),
-                              time2 = c(31,64,96,33,59,28),
-                              event = c(0,0,1,0,0,1))
+table1 <- data.frame(id = c(1,1,1,1,2,2,2,3,3),
+                     time = c(0,31,64,96,0,33,59,0,28),
+                     event = c(0,0,0,1,0,0,1,0,1))
 
 
 table2 <- data.frame(id = c(1,1,1,2,2,3),
                               time1 = c(0,31,64,0,33,0),
                               time2 = c(31,64,96,33,59,28),
-                              event = c(0,0,1,0,0,1),
-                              age = c(46,46,46,39,39,57),
-                              meds = c(0,1,2,0,1,2))
-
-table3 <- data.frame(sid = c(1,2,3),
-                    time1 = c(0,0,0),
-                    time2 = c(96,33,28),
-                    status = c(1,0,1))
-
-table4 <- data.frame(time = c(96,33,28),
-                    status = c(1,0,1))
+                              event = c(0,0,1,0,0,1))
 
 
-table <- data.frame(id = c(1,1,1,2,2,3),
+table3 <- data.frame(id = c(1,1,1,2,2,3),
+                     time1 = c(0,31,64,0,33,0),
+                     time2 = c(31,64,96,33,59,28),
+                     event = c(0,0,1,0,0,1),
+                     age = c(46,46,46,39,39,57),
+                     meds = c(0,1,2,0,1,2))
+
+
+table4 <- data.frame(sid = c(1,2,3),
+                     time1 = c(0,0,0),
+                     time2 = c(96,33,28),
+                     status = c(1,0,1))
+
+
+table0 <- data.frame(id = c(1,1,1,2,2,3),
                     time1 = c(0,31,64,0,33,0),
                     time2 = c(31,64,96,33,59,28),
                     event1 = c(0,0,1,0,0,1),
