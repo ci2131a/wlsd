bb009e936ec4d9f08390833e04c777a29290e4b3
C <ci2131a@american.edu>
2020-07-09 10:21:42 -0400

    remove rcb format

--- R/msm.R
+++ R/msm.R
@@ -28,51 +28,24 @@ surv2msm <- function(data, id, time1, time2, event, tvars = NULL, msmevent = TRU
   #if(!is.numeric(data[[time2]])) stop("time inputs must be numeric")
   if(!is.numeric(data[[event]])) stop("event variable must be numeric")
 
-  if(!rcb2){
 
-# subset first observation of each person
-  first <- data[!duplicated(data[id]),!(names(data) %in% c(time2))]
-# rename time variable
-  names(first)[names(first)==time1] <- "time"
-# ensure the starting status is the same as the first event
-  first[,event] <- min(data[event])
-# drop start variable
-  drop <- data[,!(names(data) %in% c(time1))]
-  names(drop)[names(drop)==time2] <- "time"
-# combine the stop times with the first start time
-  newdata <- rbind(first,drop)
-# sort the data by id and time
-  sorted <- newdata[order(newdata[id], newdata["time"]),]
-
-  if(msmevent){
-    if(0 %in% min(ce)){
-      sorted[event] <- sorted[event]+1
-    }else{
-      warning("Event values not changed in msmevent")
-    }
+  first <- data[,!names(data) %in% c(time2,event)]
+  names(first)[names(first) == time1] <- "time"
+  last <- data[,!names(data) %in% c(time1,tvars)]
+  names(last)[names(last) == time2] <- "time"
+  newdata <- merge(first,last,all = TRUE)
+  if(0 %in% min(data[[event]])){
+    newdata[[event]][is.na(newdata[[event]])] <- min(data[[event]])
   }
-# returns
-  return(sorted)
-
+  if(0 %in% min(data[[event]]) & msmevent){
+      newdata[event] <- newdata[event] + 1
   }
   else{
-    first <- data[,!names(data) %in% c(time2,event)]
-    names(first)[names(first) == time1] <- "time"
-    last <- data[,!names(data) %in% c(time1,tvars)]
-    names(last)[names(last) == time2] <- "time"
-    newdata <- merge(first,last,all = TRUE)
-    if(0 %in% min(data[[event]])){
-      newdata[[event]][is.na(newdata[[event]])] <- min(data[[event]])
-    }
-    if(0 %in% min(data[[event]]) & msmevent){
-      newdata[event] <- newdata[event] + 1
-    }
-    else{
-      warning("Event values not changed in msmevent")
-    }
-
-    return(newdata)
+    warning("Event values not changed in msmevent")
   }
+
+  return(newdata)
+
 
 }
 
