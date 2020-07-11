81a3a3af6669f84750642a90ca4c6f9374916f14
Charles <ci2131a@american.edu>
2020-07-11 19:24:27 -0400

    start dplyr code for count function

--- R/count.R
+++ R/count.R
@@ -1,5 +1,61 @@
 # Functions to setup longitudinal data for count data regression
 
-#count_data <- function (data, id, time, event){
-#  args <- c(data,id)
-#}
+count_data <- function (data, id, event){
+  # input checks
+  if(missing(data)) stop("Argument to data not supplied")
+  if(missing(id)) stop("Argument to id not supplied")
+  if(missing(event)) stop("Argument to event not supplied")
+
+  if (requireNamespace("dplyr", quietly = TRUE)) {
+    ge<-dplyr::group_by(data,id)
+    sums <- dplyr::summarise_at(ge,dplyr::vars(event),sum)
+    addweight <- dplyr::mutate(ge, weight = length(id))
+    select <- dplyr::select(addweight,-event)
+    filter <- dplyr::filter(select,dplyr::row_number()==1)
+    newdata <- merge(sums,filter,by = "id")
+  } else {
+    # using base
+
+  }
+
+
+
+}
+# code for dplyr
+#ge<-dplyr::group_by(table0,"id")
+#sums <- dplyr::summarise_at(ge,dplyr::vars(event1,event2),sum)
+#addweight <- dplyr::mutate(ge, weight = length(id))
+#select <- dplyr::select(addweight,-event1,-event2)
+#filter <- dplyr::filter(select,dplyr::row_number()==1)
+#newdata <- merge(sums,filter,by = "id")
+
+
+
+
+
+
+
+#%>% <- magrittr::`%>%`
+
+
+#library(dplyr)
+#table0 `pipe`
+#  group_by(id) `pipe`
+#  summarise_at(vars(event1,event2),sum)
+
+
+a<-dplyr::group_by(table0,id)
+dplyr::summarise_at(a,dplyr::vars(event1,event2),sum)
+
+
+#table0 %>%
+#  group_by(id) %>%
+#  summarise_at(vars(event1,event2),sum)
+
+
+
+
+
+#colSums(c(table0$event1,table0$event2))
+#summarise(sum(event1,event2))
+
