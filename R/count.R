4671acf1302b48c61a2509c0efffc7e40fd28b20
Charles <ci2131a@american.edu>
2020-07-11 22:36:00 -0400

    add working code for time varying function

--- R/count.R
+++ R/count.R
@@ -1,24 +1,37 @@
 # Functions to setup longitudinal data for count data regression
 
-count_data <- function (data, id, event){
+count_data <- function (data, id, event = NULL, state = NULL, tvars = NULL, tfun = "mean"){
   # input checks
   if(missing(data)) stop("Argument to data not supplied")
   if(missing(id)) stop("Argument to id not supplied")
   if(missing(event)) stop("Argument to event not supplied")
+  if(!is.null(event)&!is.null(state)) stop("Only one argument can be given to event/state")
 
-  if (requireNamespace("dplyr", quietly = TRUE)) {
-    ge<-dplyr::group_by(data,id)
-    sums <- dplyr::summarise_at(ge,dplyr::vars(event),sum)
-    addweight <- dplyr::mutate(ge, weight = length(id))
-    select <- dplyr::select(addweight,-event)
-    filter <- dplyr::filter(select,dplyr::row_number()==1)
-    newdata <- merge(sums,filter,by = "id")
-  } else {
-    # using base
+  if(!is.null(event)){
+    if (requireNamespace("dplyr", quietly = TRUE)) {
+      ge<-dplyr::group_by(data,id)
+      sums <- dplyr::summarise_at(ge,dplyr::vars(event),sum)
+      addweight <- dplyr::mutate(ge, weight = length(id))
+      select <- dplyr::select(addweight,-event)
+      filter <- dplyr::filter(select,dplyr::row_number()==1)
+      newdata <- merge(sums,filter,by = "id")
+    } else {
+      # using base
+
+    }
+  }else if(!is.null(state)){
 
   }
 
 
+}
+
+
+
+
+
+tvarfun <- function(data,tvars,tfun){
+  aggregate(vframe[,names(vframe) %in% c("meds","state")],by=list(vframe[,"id"]), median)
 
 }
 # code for dplyr
