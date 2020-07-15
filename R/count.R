d141cda7a7adce85d8ed6bc3d69d3290ddcc5e76
C <ci2131a@american.edu>
2020-07-14 20:34:44 -0400

    finish base count function

--- R/count.R
+++ R/count.R
@@ -7,21 +7,41 @@ long2count <- function (data, id, event = NULL, state = NULL, tvars = NULL, tfun
   if(!is.null(event)&!is.null(state)) stop("Only one argument can be supplied in event/state")
 
   if(!is.null(event)){
-    if (requireNamespace("dplyr", quietly = TRUE)) {
-      ge<-dplyr::group_by(data,id)
-      sums <- dplyr::summarise_at(ge,dplyr::vars(event),sum)
-      addweight <- dplyr::mutate(ge, weight = length(id))
-      select <- dplyr::select(addweight,-event)
-      filter <- dplyr::filter(select,dplyr::row_number()==1)
-      newdata <- merge(sums,filter,by = "id")
-      newname = paste(event,".counts",sep = "")
-      newdata <- dplyr::rename(newdata, !!newname := event)
-      return(newdata)
-    } else {
-      # using base
-      return("Not done yet")
+    # for use of dplyr
+    # the below DOES work
+    # it will not accept vector events
+    #if (requireNamespace("dplyr", quietly = TRUE)) {
+    #  ge<-dplyr::group_by(data,id)
+    #  sums <- dplyr::summarise_at(ge,dplyr::vars(event),sum)
+    #  addweight <- dplyr::mutate(ge, weight = length(id))
+    #  select <- dplyr::select(addweight,-event)
+    #  filter <- dplyr::filter(select,dplyr::row_number()==1)
+    #  newdata <- merge(sums,filter,by = "id")
+    #  newname <- paste(event,".counts",sep = "")
+    #  newdata <- dplyr::rename(newdata, !!newname := event)
+    #  if(!is.null(tvars)){
+    #    tframe <- tvarfun(data,id,tvars,tfun)
+    #    newdata[tvars] <- tframe[tvars]
+    #  }
+    #  return(newdata)
+    #} else {
 
-    }
+
+      # using base instead
+      weight <- aggregate(data[id], by = data[id], length)
+      colnames(weight) <- c(id,"weight")
+      tevent <- aggregate(data[event], by = data[id], sum)
+      colnames(tevent) <- c(id, paste(event,".counts", sep = ""))
+      tvar <- data[!duplicated(data[id]),!names(data) %in% c(event,tvars)]
+      newdata1 <- merge(tevent, tvar, by = id)
+      newdata <- merge(newdata1, weight, by = id)
+      if(!is.null(tvars)){
+        tframe <- tvarfun(data,id,tvars,tfun)
+        newdata[tvars] <- tframe[tvars]
+      }
+      return(newdata)
+
+    #}
   }else if(!is.null(state)){
     return("Not done yet")
 
@@ -32,13 +52,10 @@ long2count <- function (data, id, event = NULL, state = NULL, tvars = NULL, tfun
 
 
 
-newdata <- aggregate(table0[,names(table0) %in% "meds"],by=list(table0[,"id"]), "sum")
-
-
 tvarfun <- function(data,id,tvars,tfun){
   tdata <- data[,names(data) %in% c(id, tvars)]
   newdata <- aggregate(tdata[,names(tdata) %in% tvars],by=list(data[,id]), tfun)
-  colnames(newdata)[1] <- id
+  colnames(newdata) <- c(id,tvars)
   return(newdata)
 
 }
