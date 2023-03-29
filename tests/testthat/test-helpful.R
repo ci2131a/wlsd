dfed476f33edc02dd6d77e0dab3b52d45e077ab0
ci2131a <ci2131a@american.edu>
2023-03-29 17:16:16 -0400

    added test for takefirst()

--- tests/testthat/test-helpful.R
+++ tests/testthat/test-helpful.R
@@ -1,9 +1,14 @@
 context("helpful function test cases")
 
-test_that("simple 2 events to state", {
+test_that("2 events to state", {
   expect_equal(events2state(long_events,c("event1","event2")), long_e2s)
 })
 
-test_that("simple 2 events to state more arguments", {
+test_that("2 events to state added arguments", {
   expect_equal(events2state(long_events,c("event1","event2"), number = FALSE, drop = TRUE, sep = ""), long_e2s_fct)
+})
+
+
+test_that("take first tv == 1", {
+  expect_equal(takefirst(covar_long,"id","tv",1), first_covar)
 })
