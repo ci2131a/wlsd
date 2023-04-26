a608537e2bfe7a2e3ee0eb60a630dc05549abcd7
ci2131a <ci2131a@american.edu>
2023-04-26 16:14:04 -0400

    additional tests and test data added

--- tests/testthat/setup_testdata.R
+++ tests/testthat/setup_testdata.R
@@ -1,6 +1,8 @@
 # toy data sets
 # use to test the functions
 
+# standard long sets
+
 simple_long <- data.frame(id = c(1,1,1,1,2,2,2,3,3),
                           time = c(0,31,64,96,0,33,59,0,28),
                           state = c(0,0,0,1,0,0,0,0,1))
@@ -18,6 +20,7 @@ covar_long <- data.frame(id = c(1,1,1,1,2,2,2,3,3),
                           c = c(46,46,46,46,39,39,39,57,57),
                           tv = c(0,0,1,2,0,1,2,0,2))
 
+# cp sets
 
 covar_cp <- data.frame(id = c(1,1,1,2,2,3),
                         time1 = c(0,31,64,0,33,0),
@@ -32,32 +35,45 @@ cp2long_results <- data.frame(id = c(1,1,1,1,2,2,2,3,3),
                               state = c(NA,0,0,1,NA,0,0,NA,1))
 
 
+#long2count() sets
+
+event_count <- data.frame(id = c(1,2,3),
+                            time = c(47.75,27.66667,14),
+                            state = c(1,0,1))
+
+
+event_count <- data.frame(id = c(1,1,2,2,3,3),
+                          time = c(47.75,47.75,27.66667,27.66667,14,14),
+                          state = c(3,1,3,0,1,1))
+
+
+# events2state() sets
+
+long_events <- data.frame(id = c(1,1,1,1,2,2,2,3,3),
+                          time = c(0,31,64,96,0,33,59,0,28),
+                          event1 = c(0,0,0,1,0,0,0,0,1),
+                          event2 = c(0,1,1,1,0,0,1,0,1))
+
+long_e2s <- data.frame(id = c(1,1,1,1,2,2,2,3,3),
+                       time = c(0,31,64,96,0,33,59,0,28),
+                       event1 = c(0,0,0,1,0,0,0,0,1),
+                       event2 = c(0,1,1,1,0,0,1,0,1),
+                       state = c(1,3,3,4,1,1,3,1,4))
+
+
+long_e2s_fct <- data.frame(id = c(1,1,1,1,2,2,2,3,3),
+                       time = c(0,31,64,96,0,33,59,0,28),
+                       event1 = c(0,0,0,1,0,0,0,0,1),
+                       event2 = c(0,1,1,1,0,0,1,0,1),
+                       state = factor(c("00","01","01","11","00","00","01","00","11")))
 
 
 
+# takefirst() result
 
+first_covar <- data.frame(id = c(1,1,1,2,2,3,3),
+                          time = c(0,31,64,0,33,0,28),
+                          state = c(0,0,0,0,0,0,1),
+                          c = c(46,46,46,39,39,57,57),
+                          tv = c(0,0,1,0,1,0,2))
 
-
-
-
-
-
-
-
-
-
-toydata <- data.frame(id = c(1,1,1,2,2,3),
-                    time1 = c(0,31,64,0,33,0),
-                    time2 = c(31,64,96,33,59,28),
-                    event1 = c(0,0,1,0,0,1),
-                    event2 = c(0,1,1,0,1,1),
-                    state = c(0,1,2,0,1,2),
-                    age = c(46,46,46,39,39,57),
-                    meds = c(0,1,2,0,1,2))
-
-basedata <- data.frame(id = c(1,1,1,2,2,3),
-                     base = c(0,0,0,10,10,0),
-                     time1 = c(31,64,96,33,59,28),
-                     event = c(0,0,1,0,0,1),
-                     age = c(46,46,46,39,39,57),
-                     state = c(0,1,2,0,1,2))
--- tests/testthat/test-long2.R
+++ tests/testthat/test-long2.R
@@ -1,9 +1,19 @@
 context("long2cp test cases")
 
-test_that("simple longitudinal transition", {
-
+test_that("simple longitudinal to counting process transition", {
   expect_equal(long2cp(simple_long,"id","time"), simple_cp)
+})
 
+test_that("longitudinal to counting process transition with covariates", {
+  expect_equal(long2cp(covar_long,"id","time"), covar_cp)
 })
 
 
+test_that("longitudinal to count regression transition with events", {
+  expect_equal(round(long2count(data = covar_long, id = "id", event = "state", FUN = mean),2), event_count)
+})
+
+
+test_that("longitudinal to count regression transition with states", {
+  expect_equal(round(long2count(data = covar_long, id = "id", state = "state", FUN = mean),2), state_count)
+})
