b7cbbe24b8e0d7be9f86027bcbaa5b56d12c5747
Charles Ingulli <charlesfi@outlook.com>
2025-06-17 13:58:05 -0400

    update cp2long tests

--- tests/testthat/test-cp2.R
+++ tests/testthat/test-cp2.R
@@ -1,5 +1,55 @@
 # Counting Process transition tests
 
 test_that("simple cp transition", {
-  expect_equal(cp2long(cp_data,"id","time1", "time2", "state"), cp2long_results)
+
+  df <- data.frame(
+    id = c(1,1,2),
+    time1 = c(0,1,0),
+    time2 = c(1,2,1),
+    e = c(0,1,1),
+    c = c(1,1,1),
+    t = c(2,3,2)
+  )
+
+  result <- cp2long(df, "id", "time1", "time2")
+  output <- data.frame(
+    id = c(1,1,1,2,2),
+    time = c(0,1,2,0,1),
+    e = c(NA,0,1,NA,1),
+    c = c(NA,1,1,NA,1),
+    t = c(NA,2,3,NA,2)
+  )
+
+  expect_equal(result, output)
 })
+
+test_that("cp transition w/ fill", {
+
+  df <- data.frame(
+    id = c(1,1,2),
+    time1 = c(0,1,0),
+    time2 = c(1,2,1),
+    e = c(0,1,1),
+    c = c(1,1,1),
+    t = c(2,3,2)
+  )
+
+  result <- cp2long(df, "id", "time1", "time2", fill = TRUE)
+  output <- data.frame(
+    id = c(1,1,1,2,2),
+    time = c(0,1,2,0,1),
+    e = c(NA,0,1,NA,1),
+    c = c(1,1,1,1,1),
+    t = c(NA,2,3,NA,2)
+  )
+
+  expect_equal(result, output)
+})
+
+
+#test_that("cp transition w/ error", {
+
+#})
+
+
+
